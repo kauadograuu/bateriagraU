9e55cbdc8efa97403d1a6b1f0466a79d4bbc1437
kauadograuu <melo.kaua@escola.pr.gov.br>
2025-09-16 12:17:11 -0300

    sknck

---
+++ script.js
@@ -0,0 +1,24 @@
+//Criaçaõ das constantes do sons
+
+const sons = {
+    'A' : 'boom.waw',
+    'B' : 'clap.wav',
+    'C' : 'hihat.wav',
+    'D' : 'kick.wav',
+    'E' : 'openhat.wav',
+    'F' : 'ride.wav',
+    'G' : 'snare.wav',
+    'H' : 'tink.wav',
+    'I' : 'tom.wav',
+    
+}
+
+//função para criar uma div para cada tecla
+const criarDiv = (sons) => {
+    div.classList('key');
+    div.textContent = texto;
+}
+
+
+exibir(sons);
+document.getElementById('container')
